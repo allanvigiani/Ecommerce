6a3a6f585487a33f1ccf2c3e09a79f6eb122eb73
allanvigiani <vigianiallan@gmail.com>
2022-11-10 22:24:40 -0300

    verificacao do formulario de cadastro

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -20,5 +20,6 @@ from eletronicos import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
+    path('cadastro', views.cadastro, name='cadastro'),
     path('login', views.login, name='login'),
 ]
--- eletronicos/views.py
+++ eletronicos/views.py
@@ -7,11 +7,58 @@ from django.urls import reverse_lazy
 def home(request):
     return render(request, 'home/index.html')
 
+def cadastro(request):
+
+    context = {}
+
+    if request.method == 'POST':
+
+        error = {}
+
+        nome = request.POST.get('nome', None)
+        email = request.POST.get('email', None)
+        cpf = request.POST.get('cpf', None)
+        dt_nascimento = request.POST.get('dt_nascimento', None)
+        uf = request.POST.get('uf', None)
+        cidade = request.POST.get('cidade', None)
+        endereco = request.POST.get('endereco', None)
+        login = request.POST.get('login', None)
+        senha = request.POST.get('senha', None)
+        
+        if nome == '':
+            error['nome'] = 'O campo Nome nao pode ser vazio'
+
+        if email == '':
+            error['email'] = 'O campo Email nao pode ser vazio'
+
+        if cpf == '':
+            error['cpf'] = 'O campo CPF nao pode ser vazio'
+
+        if dt_nascimento == '':
+            error['dt_nascimento'] = 'O campo Data de Nascimento nao pode ser vazio'
+
+        if cidade == '':
+            error['cidade'] = 'O campo Cidade nao pode ser vazio'
+
+        if endereco == '':
+            error['endereco'] = 'O campo Endereco nao pode ser vazio'
+
+        if login == '':
+            error['login'] = 'O campo Login nao pode ser vazio'
+
+        if senha == '':
+            error['senha'] = 'O campo Senha nao pode ser vazio'
+
+        if error:
+            context['error'] = error
+        else:
+            print('SALVA OS DADOS')
+            context['sucesso'] = 'Cadastro realizado com sucesso.'
+
+    return render(request, 'cadastro/index.html', context=context)
+
 def login(request):
     return render(request, 'login/index.html')
 
 class ClienteCreate(CreateView):
-    model = Cliente
-    fields = ['nome', 'email', 'cpf', 'dt_nascimento', 'uf', 'cidade', 'endereco', 'login', 'senha']
-    template_name = 'login/index.html'
-    success_url = reverse_lazy('home')
+    pass
